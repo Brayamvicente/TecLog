9f432a92e0e0618827c274bbd9f36b95c91252e1
Brayam Vicente <brayams.vicente@gmail.com>
2025-03-10 20:07:02 -0300

    ajustando html iniciando css

--- TecLog/obj/Debug/net8.0/TecLog.AssemblyInfo.cs
+++ TecLog/obj/Debug/net8.0/TecLog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecLog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4cb08a0762559a6bcbc84ad3f9c0f1893a345e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9a51ae306549a696f61124d7bad8b0992a976a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
